717f9eb6caf4d49e55265d14feb678ec1b5d9736
Renan Pereira <renanmol87@gmail.com>
2020-08-26 12:35:15 -0300

    feat: main page in progress

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const CategoryList: React.FC = () => {
+  return (
+    <Container>
+      <Text>CategoryList</Text>
+    </Container>
+  );
+};
+
+export default CategoryList;
--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,16 +1,83 @@
 import React from 'react';
+import { View, FlatList } from 'react-native'
 
 import { Wrapper, Container, Main } from './styles';
 
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
+import CategoryList from '../../components/CategoryList';
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean; 
+}
 
 const Following: React.FC = () => {
+  const { data, indexes } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>,
+      },
+
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <Title>FOLLOWED CATEGORIES</Title>,
+        isTitle: true,
+      },
+      { key: 'C1', render: () => <CategoryList/>, },
+
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <Title>LIVE CHANNELS</Title>,
+        isTitle: true,
+      },
+      { key: 'C2', render: () => <View/>, },
+
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <Title>CONTINUE WATCHING</Title>,
+        isTitle: true,
+      },
+      { key: 'C3', render: () => <View/>, },
+
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>OFFLINE CHANNELS</Title>,
+        isTitle: true,
+      },
+      { key: 'C4', render: () => <View/>, },
+
+    ];
+
+    const indexes: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indexes.push(index));
+
+    return {
+    data: items,
+    indexes,
+    };
+  }, []);
+
   return (
     <Wrapper>
       <Container>
         <Header />
 
-        <Main />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) =>  item.render()}
+            keyExtractor={(item) => item.key}
+            stickyHeaderIndices={indexes}
+
+            //Refresh Effect
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
